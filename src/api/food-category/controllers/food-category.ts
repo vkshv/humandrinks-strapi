80104454f7b942f2a161fc430bb5bfe0d9ceafdc
Владимир Косоухов <vladimirkosouhov@MacBook-Pro-Vladimir.local>
2025-02-11 03:19:06 +0300

    add content types for admins, food

---
+++ src/api/food-category/controllers/food-category.ts
@@ -0,0 +1,7 @@
+/**
+ * food-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::food-category.food-category');
---
+++ src/api/food-category/routes/food-category.ts
@@ -0,0 +1,7 @@
+/**
+ * food-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::food-category.food-category');
