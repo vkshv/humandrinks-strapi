e46ffef65882fde353a7bb2f8df8a455b16a46a2
Владимир Косоухов <vladimirkosouhov@MacBook-Pro-Vladimir.local>
2025-04-07 23:17:08 +0300

    yml fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'strapi',
-      script: 'npm',
+      script: './node_modules/.bin/strapi',
       args: 'start',
+      cwd: '/var/www/humandrinks-strapi',
       env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
         NODE_ENV: 'production',
       },
     },
