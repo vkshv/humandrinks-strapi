9edd200dc26c80e06d7cedee12e6ca380c1d2ab8
Владимир Косоухов <vladimirkosouhov@MacBook-Pro-Vladimir.local>
2025-04-08 19:25:47 +0300

    config fix

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,14 +3,15 @@ export default ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        baseUrl: env('CDN_URL'),
-        rootPath: env('CDN_ROOT_PATH', ''),
+        // baseUrl: env('CDN_URL'),
+        // rootPath: env('CDN_ROOT_PATH', ''),
         s3Options: {
           credentials: {
             accessKeyId: env('AWS_ACCESS_KEY_ID'),
             secretAccessKey: env('AWS_ACCESS_SECRET'),
           },
           region: env('AWS_REGION'),
+          endpoint: env('CDN_URL'),
           params: {
             signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
             Bucket: env('AWS_BUCKET'),
